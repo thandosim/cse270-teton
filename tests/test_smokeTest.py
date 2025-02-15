5aaa56501525c4765c51bd5b6cadad1703c752e2
thandosim <sim24001@byui.edu>
2025-02-15 08:58:07 +0200

    finished smoke test headless

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -9,12 +9,14 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+
 
 class TestSmokeTest():
   def setup_method(self, method):
     options = Options()
     options.add_argument("--headless=new")
-    self.driver = webdriver.Firefox(options=options)
+    self.driver = webdriver.Chrome(options=options)
     self.vars = {}
   
   def teardown_method(self, method):
@@ -47,7 +49,7 @@ class TestSmokeTest():
     self.driver.find_element(By.ID, "username").send_keys("user")
     self.driver.find_element(By.ID, "password").send_keys("nane")
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
-    assert self.driver.find_element(By.CSS_SELECTOR, ".errorMessage").text == "Invalid username and password."
+    WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
   
   def test_directoryviews(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/directory.html")
